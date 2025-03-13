402c5002556448986a946e27625fbec37b6cd0c2
dantegr <tampantelis@gmail.com>
2025-03-13 08:44:46 +0100

    declaterred list schema

--- src/models/listModel.ts
+++ src/models/listModel.ts
@@ -4,33 +4,25 @@ import mongoose from "mongoose";
 interface ICustomField {
   title: string;
   value: string | number | boolean;
-  required: boolean;
 }
 
 const CustomFieldSchema = new mongoose.Schema<ICustomField>({
   title: { type: String, required: true },
   value: { type: mongoose.Schema.Types.Mixed, required: true }, // Allows string, number, or boolean
-  required: { type: Boolean, default: false },
 });
 
 // Define Subtask Interface & Schema (Supports infinite nesting)
 interface ISubtask {
   title: string;
   done: boolean;
-  cost: number;
   required: boolean;
-  type?: string; // Optional task type
-  customFields?: ICustomField[]; // Custom fields
   subtasks?: ISubtask[]; // Self-referencing for infinite nesting
 }
 
 const SubtaskSchema = new mongoose.Schema<ISubtask>({
   title: { type: String, required: true },
   done: { type: Boolean, default: false },
-  cost: { type: Number, required: true, default: 0 },
   required: { type: Boolean, default: false },
-  type: { type: String, required: false },
-  customFields: [CustomFieldSchema], // Custom fields array
   subtasks: [this], // Self-reference for infinite nesting
 });
 
